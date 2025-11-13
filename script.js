2ebf1da1b8fb828b81624e2c991504ba61afcf18
rabin9863 <rabindhakal730@gmail.com>
2025-11-13 08:30:47 +0545

    local storage

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-$(document).ready(function () {
-  $(".box").css("opacity", "1");
-});
+document
+  .getElementById("nameForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    // Get the values from input fields
+    const firstName = document.getElementById("firstName").value;
+    const lastName = document.getElementById("lastName").value;
+
+    // Save the names in localStorage
+    localStorage.setItem("firstName", firstName);
+    localStorage.setItem("lastName", lastName);
+
+    // Redirect to the welcome page
+    window.location.href = "welcome.html";
+  });
